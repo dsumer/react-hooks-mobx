ea5c8bac77adc085ce3ccff03de09f0fde1f4fd8
Dominik Sumer <domi.sumer@gmail.com>
2019-02-02 20:17:05 +0100

    example of using mobx together with hooks and context

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,65 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { Observer } from "mobx-react-lite";
+import { PhaseStore, PhaseStoreContext } from "./stores/PhaseStore";
 import logo from './logo.svg';
 import './App.css';
 
+function Test() {
+  const phaseStore = useContext(PhaseStoreContext);
+
+  return (
+    <div>
+      test <button onClick={phaseStore.toggleHint}>doit</button>
+      <button onClick={phaseStore.addPhase}>Add Phase</button>
+      <Observer>
+        {() => (
+          <div>
+            {phaseStore.phases.map((phase) => <div>{phase.name}</div>)}
+          </div>
+        )}
+      </Observer>
+    </div>
+  );
+}
+
+function OtherTest() {
+  const phaseStore = useContext(PhaseStoreContext);
+
+  return (
+    <div>
+      othertest&nbsp;
+      <Observer>
+        {() => (
+          <React.Fragment>{phaseStore.showHint.toString()}</React.Fragment>
+        )}
+      </Observer>
+    </div>
+  );
+}
+
 function App() {
+  const [phaseStore, setPhaseStore] = useState(new PhaseStore());
+
+  function loadPhaseStoreFromJson() {
+    const json = {
+      showHint: true,
+      phases: [
+        {
+          name: 'lol'
+        },
+        {
+          name: 'hehe'
+        }
+      ]
+    };
+
+    setPhaseStore(PhaseStore.deserialize(json));
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -18,6 +71,11 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={loadPhaseStoreFromJson}>Load From Json</button>
+        <PhaseStoreContext.Provider value={phaseStore}>
+          <Test/>
+          <OtherTest/>
+        </PhaseStoreContext.Provider>
       </header>
     </div>
   );
---
+++ src/stores/PhaseStore.ts
@@ -0,0 +1,39 @@
+import React from "react";
+import { action, observable } from "mobx";
+
+class Phase {
+  public id: number;
+  @observable public name: string;
+  public date: Date;
+
+  constructor(data: any) {
+    this.id = data.id;
+    this.name = data.name;
+    this.date = data.date;
+  }
+}
+export class PhaseStore {
+  @observable public showHint = false;
+  @observable public phases: Phase[] = [];
+
+  @action.bound
+  toggleHint() {
+    this.showHint = !this.showHint;
+  }
+
+  @action.bound
+  addPhase() {
+    this.phases.push(new Phase({name: 'oha'}));
+  }
+
+  static deserialize(data: any) {
+    const phaseStore = new PhaseStore();
+    phaseStore.showHint = data.showHint;
+    if (data.phases) {
+      phaseStore.phases = data.phases.map((phase: any) => new Phase(phase));
+    }
+    return phaseStore;
+  }
+}
+
+export const PhaseStoreContext = React.createContext(new PhaseStore());
